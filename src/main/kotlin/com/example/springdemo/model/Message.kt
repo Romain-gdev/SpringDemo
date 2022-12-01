7c42cc61addfbce31ad0644afcdd500e6035a091
E199291W <romain.gautier@tu.univ-nantes.fr>
2022-12-01 12:16:58 +0100

    doc

--- src/main/kotlin/com/example/springdemo/model/Message.kt
+++ src/main/kotlin/com/example/springdemo/model/Message.kt
@@ -3,5 +3,9 @@ package com.example.springdemo.model
 import javax.persistence.Entity
 import javax.persistence.Id
 
+/**
+ * @property id String? Message id
+ * @property text String Message text
+ */
 @Entity
 data class Message(@Id val id : String?, val text: String)
