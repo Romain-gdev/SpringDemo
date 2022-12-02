1aaa74c6fb35235d8c77ef7527059d15fcad9a9d
E199291W <romain.gautier@tu.univ-nantes.fr>
2022-12-02 09:35:40 +0100

    tests

---
+++ src/main/kotlin/com/example/springdemo/service/TaskService.kt
@@ -0,0 +1,17 @@
+package com.example.springdemo.service
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class TaskService {
+    
+    fun execute(task: String){
+        logger.info("do $task")
+    }
+
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(TaskService::class.java)
+    }
+}
